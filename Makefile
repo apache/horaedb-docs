17d2e82ae31e12634ecca7da91e79254dca1d0b0
tison <wander4096@gmail.com>
2023-12-25 18:51:28 +0800

    ci: deploy website
    Signed-off-by: tison <wander4096@gmail.com>

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 install:
-	cargo install mdbook@0.4.25
-	cargo install mdbook-i18n --git https://github.com/chunshao90/mdbook-i18n.git --rev ca497cff369e0a5cedcd4024af6e1f05cc5050c5
+	cargo install mdbook@0.4.36
+	cargo install mdbook-i18n
 
 serve:
 	cd docs && mdbook serve
 
 build:
-	cd docs && mdbook build
+	cd docs && mdbook build && ./move.sh
 
 lint:
 	find . -name '*.md' | xargs npx prettier@2.7.1 --write
--- docs/move.sh
+++ docs/move.sh
@@ -1,6 +1,15 @@
-#!/bin/bash
-echo "ready to move files"
-cp -r ../i18n/cn ../html/cn
-cp -r ../i18n/en ../html/en
-cp -r ../../src/resources ../../book/html/resources
-echo "copy done"
+#!/usr/bin/env bash
+
+set -exo
+
+cd $( dirname ${0} ) >/dev/null 2>&1
+
+echo "Moving files ..."
+
+cp -R book/i18n/cn book/html/cn
+cp -R book/i18n/en book/html/en
+cp -R resources book/html/resources
+
+cp ../.asf.yaml book/html/.asf.yaml
+
+echo "Files has been moved."
